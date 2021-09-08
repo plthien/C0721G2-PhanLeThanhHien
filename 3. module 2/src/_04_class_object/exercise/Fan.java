6153b7d560e03aad187cc288d7f1be0083cd31b6
plthien <plthienbkdn@gmail.com>
2021-09-08 07:03:43 +0700

    update fan

--- 3. module 2/src/_04_class_object/exercise/Fan.java
+++ 3. module 2/src/_04_class_object/exercise/Fan.java
@@ -1,10 +1,8 @@
 package _04_class_object.exercise;
 
 public class Fan {
-    final int SLOW = 1;
-    final int MEDIUM = 2;
-    final int FAST = 3;
-    private int speed = SLOW;
+
+    private int speed = 1;
     private boolean on = false;
     private double radius = 5;
     private String color = "blue";
@@ -29,19 +27,7 @@ public class Fan {
     }
 
     public void setSpeed(int speed) {
-        switch (speed) {
-            case 1:
-                this.speed = SLOW;
-                break;
-            case 2:
-                this.speed = MEDIUM;
-                break;
-            case 3:
-                this.speed = FAST;
-                break;
-        }
-
-
+        this.speed = speed;
     }
 
     public void setOn(boolean on) {
--- 3. module 2/src/_04_class_object/exercise/FanMain.java
+++ 3. module 2/src/_04_class_object/exercise/FanMain.java
@@ -3,17 +3,20 @@ package _04_class_object.exercise;
 
 public class FanMain {
     public static void main(String[] args) {
+        final int SLOW = 1;
+        final int MEDIUM = 2;
+        final int FAST = 3;
         Fan fan1 = new Fan();
         Fan fan2 = new Fan();
         fan1.setOn(true);
         fan1.setRadius(10);
         fan1.setColor("Yellow");
-        fan1.setSpeed(3);
+        fan1.setSpeed(FAST);
         System.out.println(fan1.toString());
         fan2.setOn(false);
         fan2.setRadius(5);
         fan2.setColor("Blue");
-        fan2.setSpeed(2);
+        fan2.setSpeed(MEDIUM);
         System.out.println(fan2.toString());
     }
 
