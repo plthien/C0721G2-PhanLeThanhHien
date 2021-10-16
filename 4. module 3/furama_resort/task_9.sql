0f6cb4b100a775782b41165368076cdb209c97bd
plthien <plthienbkdn@gmail.com>
2021-10-16 09:40:52 +0700

    update task9

--- 4. module 3/furama_resort/task_9.sql
+++ 4. module 3/furama_resort/task_9.sql
@@ -1,13 +1,13 @@
 use furama_resort_management;
 
-select  ctr1.`month`, ctr1.number_of_contract, ctr1.cost
--- ,sum(ctrd.quantity*exs.price) + f.cost as total
-from ( 
-		select ctr.id , date_format(ctr.check_in_date,'%m-%Y') as `month`, count(ctr.id) as number_of_contract, ctr.facility_id, sum(f.cost) as cost
-		from contract ctr 	join facility f on f.id = ctr.facility_id
-        group by `month`
-    ) ctr1
---     join contract_detail ctrd on ctrd.contract_id = ctr1.id
---     join extra_service exs on exs.id = ctrd.extra_service_id
-
+select  date_format(ctr.check_in_date,'%m-%Y') as `month`, count(ctr.id) as number_of_contract, sum(f.cost + ctrd2.cost_2) as total
+from contract ctr 	
+	join facility f on f.id = ctr.facility_id
+	join ( 
+		select ctrd.contract_id, sum(ctrd.quantity*exs.price) as cost_2
+        from  contract_detail ctrd join extra_service exs on exs.id = ctrd.extra_service_id
+        group by ctrd.contract_id
+    ) ctrd2 on ctr.id = ctrd2.contract_id
+where year(ctr.check_in_date) = '2019'
+group by `month`
 
