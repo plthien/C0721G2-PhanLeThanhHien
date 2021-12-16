0b992e0d81fe428542614c90673a4691944191a5
plthien <plthienbkdn@gmail.com>
2021-12-16 15:37:15 +0700

    update typescript

---
+++ 6. module 5/2. typescript/exercise/typescript-fibonacci/src/main.ts
@@ -0,0 +1,14 @@
+function getFibonacci(n:number): number {
+    if (n==1 || n==2)
+        return 1;
+    return getFibonacci(n-1) + getFibonacci(n - 2);
+}
+
+let n:number = 20;
+let sum:number = 0;
+console.log("Dãy " + n + " số Fibonacci đầu tiên: ");
+for (let i = 1; i <=n; i++){
+    console.log(getFibonacci(i) + " ");
+    sum+=getFibonacci(i);
+}
+console.log("Tổng " + n + " số Fibonacci đầu tiên là: " + sum);
