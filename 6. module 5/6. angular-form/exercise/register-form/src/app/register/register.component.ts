05e18033e524c89e9e2f09d88f526998ef0d01a4
plthien <plthienbkdn@gmail.com>
2021-12-24 09:38:55 +0700

    update custom validate

--- 6. module 5/6. angular-form/exercise/register-form/src/app/register/register.component.ts
+++ 6. module 5/6. angular-form/exercise/register-form/src/app/register/register.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+export function comparePassword(c: AbstractControl) {
+  const v = c.value;
+  return (v.password === v.cPassword) ? null : {passwordnotmatch: true};
+}
 
 @Component({
   selector: 'app-register',
@@ -9,21 +14,26 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 export class RegisterComponent implements OnInit {
   userForm: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) { }
+  constructor(private _formBuilder: FormBuilder) {
+  }
 
   ngOnInit(): void {
     this.userForm = this._formBuilder.group({
-      email: ['',[Validators.required,Validators.email]],
-      password: ['',[Validators.required,Validators.minLength(6)]],
-      cPassword:['',[Validators.required,Validators.minLength(6)]],
-      country:['',[Validators.required]],
-      age:['',[Validators.required, Validators.min(18)]],
-      gender:['Male',[Validators.required]],
-      phoneNumber:['',[Validators.required,Validators.pattern('^\\+84\\d{9,10}$')]]
-    })
+      email: ['', [Validators.required, Validators.email]],
+      country: ['', [Validators.required]],
+      age: ['', [Validators.required, Validators.min(18)]],
+      gender: ['Male', [Validators.required]],
+      phoneNumber: ['', [Validators.required, Validators.pattern('^\\+84\\d{9,10}$')]],
+      pwGroup: this._formBuilder.group({
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        cPassword: ['', [Validators.required]]
+      }, {
+        validator: comparePassword
+      }),
+    });
   }
 
   onSubmit() {
-    console.log(this.userForm.value)
+    console.log(this.userForm.value);
   }
 }
